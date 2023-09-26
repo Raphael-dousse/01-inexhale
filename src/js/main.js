3c67024b6187bbc87420a35f66eca8e3b39316fc
Raphael-dousse <98466908+Raphael-dousse@users.noreply.github.com>
2023-09-26 15:17:44 +0200

    commit-1

---
+++ src/js/main.js
@@ -0,0 +1,16 @@
+import { gsap } from "gsap";
+
+document.addEventListener("DOMContentLoaded", function () {
+
+  const monH2 = document.getElementById("monH2");
+  const monSVG = document.querySelector("monH3");
+
+ 
+
+    }
+
+  );
+
+});
+
+ 
